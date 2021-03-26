a2f008ff17e3023d44dfbe0277735544e4fd6418
Emmy Ma <hma@babson.edu>
2021-03-26 12:05:13 -0400

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template, redirect
 from forms import GeoForm
-
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
@@ -12,8 +12,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = GeoForm()
+    if form.is_submitted():
+        return render_template('mbta.html')
     return render_template('index.html', form=form)
 
+@app.route('/mbta')
+def mbta():
+    return render_template('mbta.html')
 
 
 if __name__ == "__main__":
